cd7845e35b74f3fefad37b8fe47d25e5cee0b016
Nnelo <broudicelouann@gmail.com>
2025-10-12 22:15:13 +0200

    create window and install glad for future

--- Makefile
+++ Makefile
@@ -1,23 +1,29 @@
 NAME = scop
 
 CC = c++
-CFLAGS = -Wall -Wextra -Werror -g -std=c++21
+CFLAGS = -Wall -Wextra -Werror -g -std=c++17
+INCLUDES = -Iincludes
 
 OBJDIR = obj
 
 SRCS = srcs/main.cpp
 OBJS = $(SRCS:%.cpp=$(OBJDIR)/%.o)
+OBJS := $(OBJS:%.c=$(OBJDIR)/%.o)
 
 all: $(NAME)
 
 $(OBJDIR):
 	@mkdir -p $(OBJDIR)
+	@mkdir -p $(OBJDIR)/srcs
 
 $(OBJDIR)/%.o: %.cpp | $(OBJDIR)
-	$(CC) $(CFLAGS) -c $< -o $@
+	$(CC) $(CFLAGS) $(INCLUDES) -c $< -o $@
+
+$(OBJDIR)/%.o: %.c | $(OBJDIR)
+	$(CC) $(CFLAGS) $(INCLUDES) -c $< -o $@
 
 $(NAME): $(OBJS)
-	$(CC) $(CFLAGS) $(OBJS) -o $(NAME)
+	$(CC) $(CFLAGS) $(OBJS) -lglfw -ldl -lGL -o $(NAME)
 	@echo "\033[32m✔ Compilation completed\033[0m"
 
 clean:
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,1 +1,42 @@
-#include <
+#include <GLFW/glfw3.h>
+// #include <glad/glad.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int heigth)
+{
+	(void)window;
+	glViewport(0, 0, width, heigth);
+}
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow *window = glfwCreateWindow(800, 600, "Scop", NULL, NULL);
+	if (window == NULL) {
+		std::cerr << "failed to create window\n";
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+	// 	std::cerr << "failed to initialize GLAD\n";
+	// 	return -1;
+	// }
+
+	glViewport(0, 0, 800, 600);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
+	return 0;
+}
